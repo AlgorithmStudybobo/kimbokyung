47b750bc055ee3597f0b92b95f723d7dc7b4a4c0
kim bo kyung <79852928+bobokyung@users.noreply.github.com>
2023-08-22 11:05:23 +0900

    Create BJ_sensor.java

---
+++ 230822/BJ_sensor.java
@@ -0,0 +1,67 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int N,K,input[],arr[],min,dis[];
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        K = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        Set<Integer> set = new HashSet<>();
+        for (int i = 0; i < N ; i++) {
+            set.add(Integer.parseInt(st.nextToken()));
+        }
+        if(N<=K){
+            System.out.println(0); return;
+        }
+        input = new int[K];
+        arr = new int[set.size()]; int i = 0;
+        for (int s : set) {
+            arr[i++] = s;
+        }
+        Arrays.sort(arr);
+        dis = new int[arr.length-1];
+        dis[0] = arr[1]-arr[0];
+        for (int j = 1; j < arr.length-1; j++) {
+            dis[j] = arr[j+1]-arr[j];
+        }
+        Arrays.sort(dis);
+        int sum=0;
+        for (int j = 0; j < arr.length-K; j++) {
+            sum += dis[j];
+        }
+        System.out.println(sum);
+    }
+//    static void select(int cnt,int start) {
+//        if (cnt == K) {
+//            int cost = arr[input[0]]-arr[0];
+//            for (int i = 1; i < K; i++) {
+//                cost += arr[input[i]]-arr[input[i-1]];
+//            }
+//            min = Math.min(min,cost);
+//            return;
+//        }
+//        for (int i = start; i<arr.length-1; i++) {
+//            input[cnt] = i;
+//            select(cnt + 1,i+1);
+//        }
+//    }
+
+//    private static void distance() {
+//        int k = 0;
+//        int sum = 0;
+//        for (int i = 0; i < arr.length; i++) {
+//
+//            while(input[k]<=i){
+//                if(k>=input.length-1) break;
+//                k++;
+//            }
+//            if(i==input[k]) continue;
+//            if(k==0) sum += Math.abs(arr[input[k]]-arr[i]);
+//            else sum += Math.min(Math.abs(arr[input[k]]-arr[i]), Math.abs(arr[i]-arr[input[k-1]]));
+//        }
+//        min = Math.min(min,sum);
+//    }
+}
+// 조합 (시간초과) --> 좌 우 거리 비교 후 최대 값 삭제 ( 틀림 ) --> 정민찬스 ^.^
